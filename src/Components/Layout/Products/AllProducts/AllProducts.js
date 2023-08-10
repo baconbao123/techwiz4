4f0ca92c428e4e8df52c76e5b9ed50a761c8da3c
Yahbini <ltpa1203@gmail.com>
2023-08-10 16:42:00 +0700

    PA commit select

--- src/Components/Layout/Products/AllProducts/AllProducts.js
+++ src/Components/Layout/Products/AllProducts/AllProducts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col, Button, Form, Dropdown } from 'react-bootstrap'
 import bannerallpro from '../../../../assets/Layout_img/banner-allpro.png'
 import './AllProducts.scss'
@@ -14,28 +14,95 @@ console.log(productList);
 const AllProducts = () => {
     const [categoryName, setCategoryName] = useState('All Product')
     const [searchValue, setSearchValue] = useState('')
-    const [currentDisplayProduct, setCurrentDisplayProduct] = useState(8)
+    const [option, setOption] = useState(0)
+    const AllProduct=productList;
+    const [product,setProduct] = useState(productList)
+    const [bonsai,setBonsai]=useState([]);
+    const [flower,setFlower]=useState([]);
+    const [indoor,setIndoor] = useState([]);
+    const [outdoor,setOutdoor]=useState([]);
+    const [succulent,setScculent]=useState([]);
+    console.log("Check indoor",indoor);
+   
+    console.log("Check flower",flower);
+    console.log("Check outdoor",outdoor);
+    console.log("Check succulent",succulent);
+    console.log("Check bonsai",bonsai);
+    useEffect(()=> {
+        let indoorArr=[];
+        let outdoorArr=[];
+        let flowerArr=[];
+        let bonSaiArr=[];
+        let succulentArr=[];
+        productList.filter( (item)=>{
+            if(item.category==='indoor'){
+              indoorArr.push(item);
+              
+            }
+            else if (item.category==='outdoor') {
+                outdoorArr.push(item);
+              
+            }
+            else if (item.category==='floweringshrubs') {
+                flowerArr.push(item);
+              
+            }
+            else if (item.category==='bonsai') {
+                bonSaiArr.push(item);
+               
+            }
+            else if (item.category==='succulents') {
+                succulentArr.push(item);
+            }
+        })
+    
+    setIndoor(indoorArr)
+    setOutdoor(outdoorArr)
+    setFlower(flowerArr)
+    setBonsai(bonSaiArr)
+    setScculent(succulentArr)
+
+    },[])
+    const [currentDisplayProduct, setCurrentDisplayProduct] = useState(8);
+    const handelChange=(e)=> {
+     
+        if(e.target.value==='1') {
+            console.log("BONSAI");
+            setProduct(bonsai)
+        }
+        else  if(e.target.value==='2') {
+            setProduct(flower)
+        }
+        else  if(e.target.value==='3') {
+            setProduct(indoor)
+        }
+        else  if(e.target.value==='4') {
+            setProduct(outdoor)
+        }
+        else  if(e.target.value==='5') {
+            setProduct(succulent)
+        }
+        else  if(e.target.value==='Category') {
+            setProduct(AllProduct)
+        }
+    }
 
 
 
     return (
 
         <div className="allproduct-master">
-            <div className="banner-allproduct">
-                <img src={bannerallpro} className="img-banner" />
-                <span className="name-cataegory">{categoryName}</span>
-            </div>
             <div className="allproduct-div">
                 <div className="container">
                     <Row className="filter-div">
                         <Col lg={2}>
-                            <Form.Select className="form-select-catalog" aria-label="Category">
+                            <Form.Select className="form-select-catalog" aria-label="Category" onChange={e=>handelChange(e)} >
                                 <option checked>Category</option>
-                                <option>Bonsai</option>
-                                <option>Flowering Shrubs</option>
-                                <option>Inddor</option>
-                                <option>Outdoor</option>
-                                <option>Succulents</option>
+                                <option value={1}>Bonsai</option>
+                                <option value={2}>Flowering Shrubs</option>
+                                <option value={3}>Indoor</option>
+                                <option value={4}>Outdoor</option>
+                                <option value={5}>Succulents</option>
                             </Form.Select>
                         </Col>
                         <Col lg={2}>
@@ -55,7 +122,7 @@ const AllProducts = () => {
 
                         <div className="card-of-cate">
                             <Row>
-                                {productList.map((item, index) => (
+                                {product.map((item, index) => (
                                     <Col lg={3} key={index} className="mt-4">
                                         <CardProduct item={item} />
                                     </Col>
--- src/Components/Layout/Products/ProductDetails/CardProduct/CardProduct.js
+++ src/Components/Layout/Products/ProductDetails/CardProduct/CardProduct.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { FaRegHeart } from 'react-icons/fa'
 import './CardProduct.scss'
 
 export const CardProduct = ({item}) => {
     return (
-        <Card className="card-product">
-            <Card.Title className="card-title-product">
-            {item.tag ? <span className="span-best-seller">{item.tag}</span> : null}
-                <div className="card-name-catergory-product">
-                    <span className="card-name-product">{item.name}</span>
-                    <span className="card-catergory-product">Catergory: {item.category}</span>
-                </div>
-                <span className="icon-wishlist">
-                    <FaRegHeart />
-                </span>
-            </Card.Title>
-            <Card.Img className='card-img-1' src={item.img[0].img1} />
-            <Card.Body>
-                <div className="card-price-shopping-product">
-                    <span className="card-price-product">${item.price}</span>
-                    <span className="card-shopping-product">Add to card</span>
-                </div>
-            </Card.Body>
-        </Card>
+        <Link to='/all'>
+            <Card className="card-product">
+                <Card.Title className="card-title-product">
+                {item.tag ? <span className="span-best-seller">{item.tag}</span> : null}
+                    <div className="card-name-catergory-product">
+                        <span className="card-name-product">{item.name}</span>
+                        <span className="card-catergory-product">Catergory: {item.category}</span>
+                    </div>
+                    <span className="icon-wishlist">
+                        <FaRegHeart />
+                    </span>
+                </Card.Title>
+                <Card.Img className='card-img-1' src={item.img[0].img1} />
+                <Card.Body>
+                    <div className="card-price-shopping-product">
+                        <span className="card-price-product">${item.price}</span>
+                        <span className="card-shopping-product">Add to card</span>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Link>
 
     )
 }
