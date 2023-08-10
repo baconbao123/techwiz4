51d822a77a788838633260d9ad280827f8cddc86
baconbao123 <anh450460@gmail.com>
2023-08-10 10:34:09 +0700

    Long

--- src/Components/Layout/TopBar/BottomBar.jsx
+++ src/Components/Layout/TopBar/BottomBar.jsx
@@ -1,43 +1,35 @@
-import React from 'react'
-import './BottomBar.scss'
-import { AiOutlineSearch, AiOutlineHeart, AiOutlineShoppingCart, AiOutlineUser, AiOutlineMenuUnfold, } from "react-icons/ai";
-import {Container,Row} from 'react-bootstrap'
+import React from "react";
+import "./BottomBar.scss";
+import {
+  AiOutlineSearch,
+  AiOutlineHeart,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineMenuUnfold,
+} from "react-icons/ai";
+import { Container, Row } from "react-bootstrap";
 export default function BottomBar() {
   return (
-    <Container fuild className='bottom-bar'>
-       
-      
-            <section className='item-bottom '>
+    <Container fuild className="bottom-bar d-none">
+      <section className="item-bottom ">
         <div>
-        <AiOutlineHeart/>
-          
-        
-          <div >
-          Favorite
-          </div>
+          <AiOutlineHeart />
+
+          <div>Favorite</div>
         </div>
-        
-            </section>
-            <section className='item-bottom '>
-                <div>
-                    
-          <AiOutlineShoppingCart/>
-               <div>
-               Cart
-               </div>
-                </div>
-            </section>
-            <section className='item-bottom '>
-                <div>
-                    
-           <AiOutlineUser />
-           <div>
-           User
-           </div>
-                </div>
-          
-            </section>
-       
+      </section>
+      <section className="item-bottom ">
+        <div>
+          <AiOutlineShoppingCart />
+          <div>Cart</div>
+        </div>
+      </section>
+      <section className="item-bottom ">
+        <div>
+          <AiOutlineUser />
+          <div>User</div>
+        </div>
+      </section>
     </Container>
-  )
+  );
 }
